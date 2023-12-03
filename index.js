48ae7a4f83514920f3d27cc6e14f75294a373548
cerramaximiliano <cerramaximiliano@gmail.com>
2023-12-03 11:38:19 -0300

    Back End Development 1/5

--- index.js
+++ index.js
@@ -18,18 +18,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/2015-12-25", (req, res) => {
+app.get("/api/:date", (req, res) => {
+  const { date } = req.params;
+  const newDate = new Date (date)
   res.json({
-    "unix": "1451001600000",
-    "utc": "Fri, 25 Dec 2015 00:00:00 GMT"
+    "unix": date,
+    "utc": newDate
     })
 });
-app.get("/api/1451001600000", (req, res) => {
-  res.json({
-    "unix": "1451001600000",
-    "utc": "Fri, 25 Dec 2015 00:00:00 GMT"
-    })
-});
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
